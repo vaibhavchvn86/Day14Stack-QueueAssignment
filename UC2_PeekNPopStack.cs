bae5faef6f5638698ac3c04614a9410db06ff142
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-30 14:29:00 +0530

    [VaibhavChavan]Added:Peek and Pop Stack Linkedlist

---
+++ UC2_PeekNPopStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Day14StackNQueueAssg
+{
+    class UC2_PeekNPopStack
+    {
+        private Node top;
+        public UC2_PeekNPopStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} inserted into Stack LinkedList", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of stack", this.top.data);
+        }
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty,Deletion is not Possible");
+                return;
+            }
+            Console.WriteLine("Value Poped is {0}", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void IsEmpty()
+        {
+            while(this.top!=null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+ }
+}
