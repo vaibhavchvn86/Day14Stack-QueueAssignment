cfcf0f85c02d1ddff3673eacf1a4048b76ba984c
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-30 14:31:41 +0530

    [VaibhavChavan]Added:Dequeue a Queue Linkedlist

---
+++ UC4_Dequeue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14StackNQueueAssg
+{
+    class UC4_Dequeue
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+            temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue",node.data);
+        }
+        internal void Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("\nQueue is Empty");
+            }
+            else
+            {
+                Console.Write("\n{0} Dequeued For Queue",head.data);
+                head = head.next;
+            }
+        }
+        internal void Display()
+        {
+            Node temp = head;
+            if(temp==null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }    
+            while(temp!=null)
+            {
+                Console.Write(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+}
