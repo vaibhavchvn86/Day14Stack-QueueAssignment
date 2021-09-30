41819b5d80ac402b053d68fb87f96ebf501fd587
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-30 14:27:16 +0530

    [VaibhavChavan]Added:Create a Stack Linkedlist

---
+++ UC1_CreateStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14StackNQueueAssg
+{
+    class UC1_CreateStack
+    {
+        private Node top;
+        public UC1_CreateStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int Value)
+        {
+            Node node = new Node(Value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} inserted into Stack",Value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while(temp!=null)
+            {
+                Console.Write(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+}
